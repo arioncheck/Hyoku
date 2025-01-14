97ec0b2499c9598cfa6f8fc19a5a29ea12101d12
arion <avuvvvvaaa@gmail.com>
2025-01-14 22:06:29 +0300

    Update hikka_backup.py

--- hikka/modules/hikka_backup.py
+++ hikka/modules/hikka_backup.py
@@ -65,7 +65,7 @@ class HikkaBackupMod(loader.Module):
             "📼 Your database backups will appear here",
             silent=True,
             archive=True,
-            avatar="https://0x0.st/s/Yd5FYeoKDWdpSkP7C8xMyw/8oTo.jpg",
+            avatar="https://0x0.st/s/yCYelQJNdkxdAWXw6mN1-w/8oT-.jpg",
             _folder="hikka",
         )
 
