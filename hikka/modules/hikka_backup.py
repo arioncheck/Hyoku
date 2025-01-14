843f045adbd5a9025eb8caf396b6f90f904185e2
arion <avuvvvvaaa@gmail.com>
2025-01-14 22:00:25 +0300

    Update hikka_backup.py

--- hikka/modules/hikka_backup.py
+++ hikka/modules/hikka_backup.py
@@ -33,7 +33,7 @@ class HikkaBackupMod(loader.Module):
         if not self.get("period"):
             await self.inline.bot.send_photo(
                 self.tg_id,
-                photo="https://github.com/hikariatama/assets/raw/master/unit_alpha.png",
+                photo="https://0x0.st/s/_BI4EBtEmusaP77Mgo1gQg/8oTB.jpg",
                 caption=self.strings("period"),
                 reply_markup=self.inline.generate_markup(
                     utils.chunks(
